961c506c10caef9635965eaad9d6d8c06244312b
strangetitan <140270568+strangetitan@users.noreply.github.com>
2023-07-27 21:36:29 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember()
+{
+    var member = new Object();
+    member.name = "Sally";
+    member.age = 25;
+    member.location = "Boston";
+    member.skills = ["JavaScript", "HTML", "CSS"];
+    return member;
+}
